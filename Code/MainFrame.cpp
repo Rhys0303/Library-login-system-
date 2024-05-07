ddf9bdb1f447ddb9c0483035b941f4423208447d
Triscoc <142778724+Triscoc@users.noreply.github.com>
2024-05-08 01:50:14 +0800

    Update MainFrame.cpp

--- Code/MainFrame.cpp
+++ Code/MainFrame.cpp
@@ -6,11 +6,8 @@
 #include <sstream>
 
 MainFrame::MainFrame(const wxString& title, LoginFrame* loginFrame) : wxFrame(nullptr, wxID_ANY, title) {
-	ID = loginFrame->getID();
 	loginFrame->Destroy();
 
-	wxPanel* panel = new wxPanel(this);
-	wxStaticText* libraryText = new wxStaticText(panel, wxID_ANY, "My Library", wxPoint(10,20), wxSize(120,-1));
 }
 
 
