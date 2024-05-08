bdc9f94862f2c580f7d6852834e76152655ab7fb
Rhys0303 <164986837+Rhys0303@users.noreply.github.com>
2024-05-08 10:01:24 +0800

    Update MainFrame.cpp

--- Code/MainFrame.cpp
+++ Code/MainFrame.cpp
@@ -1,9 +1,6 @@
 #include "MainFrame.h"
 #include "LoginFrame.h"
 #include <wx/wx.h>
-#include <wx/listctrl.h>
-#include <fstream>
-#include <sstream>
 
 MainFrame::MainFrame(const wxString& title, LoginFrame* loginFrame) : wxFrame(nullptr, wxID_ANY, title) {
 	loginFrame->Destroy();
